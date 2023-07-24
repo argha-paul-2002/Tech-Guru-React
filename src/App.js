36349507e0bc9f6edd9d23eb7ffa71e1615917ec
Argha Paul <arghapaul002@gmail.com>
2023-07-24 23:15:03 +0530

    Some small modification and new files added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Header from "./components/Header";
+import "./styles/App.scss";
+
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route />
       </Routes>
