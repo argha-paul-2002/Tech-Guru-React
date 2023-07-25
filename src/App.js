9a2dc131d3aa16d312c36b969f36bddd6926e2fe
Argha Paul <arghapaul002@gmail.com>
2023-07-25 23:01:26 +0530

    Home page and contact page design completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+
+
 import "./styles/App.scss";
+import "./styles/Header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@All rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://youtube.com/" target='blank'>Youtube</a>
+                <a href="https://instagram.com/" target='blank'>Instagram</a>
+                <a href="https://github.com/" target='blank'>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
